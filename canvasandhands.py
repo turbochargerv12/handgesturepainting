f29175597c670f4536560ea61323dd67b38af75a
Roshan Reji <roshanreji2023@it.ajce.in>
2023-02-10 11:06:10 +0530

    2

--- canvasandhands.py
+++ canvasandhands.py
@@ -1,48 +1,45 @@
 import cv2
+import numpy as np
 import tkinter as tk
+from PIL import Image, ImageTk
 
-# Load the hand cascade classifier
-hand_cascade = cv2.CascadeClassifier('hand.xml')
+# Function to receive output from MediaPipe and display on canvas
+def display_mediapipe_output(canvas, visualizer_options, visualization_stream):
+    while True:
+        packets = visualization_stream.Next()
+        if not packets:
+            break
+        for packet in packets:
+            if packet.index == 1:
+                image = packet.GetValue()
+                color_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+                height, width, _ = color_image.shape
+                image = np.array(color_image)
+                image = Image.fromarray(image)
+                image = ImageTk.PhotoImage(image)
+                canvas.create_image(0, 0, image=image, anchor=tk.NW)
+                canvas.pack()
 
-# Start the OpenCV video capture
-cap = cv2.VideoCapture(0)
+# Function to display multiple windows on canvas
+def display_multiple_windows():
+    root = tk.Tk()
+    root.title("MediaPipe Output")
+    canvas_width = 720
+    canvas_height = 720
 
-# Create the Tkinter window and canvas
-root = tk.Tk()
-canvas = tk.Canvas(root, width=800, height=600, bg='white')
-canvas.pack()
+    # Create 3 canvas for different outputs
+    canvas1 = tk.Canvas(root, width=canvas_width, height=canvas_height)
+    canvas1.pack(side="left")
+    canvas2 = tk.Canvas(root, width=canvas_width, height=canvas_height)
+    canvas2.pack(side="left")
+    canvas3 = tk.Canvas(root, width=canvas_width, height=canvas_height)
+    canvas3.pack(side="left")
 
-# Function to update the canvas pointer position
-def update_pointer_position(x, y):
-    canvas.coords(pointer, x, y, x + 10, y + 10)
+    # Call display_mediapipe_output function to receive output from MediaPipe
+    display_mediapipe_output(canvas1, None, visualization_stream)
+    display_mediapipe_output(canvas2, None, visualization_stream)
+    display_mediapipe_output(canvas3, None, visualization_stream)
+    root.mainloop()
 
-# Create the canvas pointer
-pointer = canvas.create_oval(0, 0, 10, 10, fill='red')
-
-# Function to process video frames
-def process_frame():
-    # Read a video frame from the OpenCV capture
-    ret, frame = cap.read()
-
-    # Convert the frame to grayscale
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    # Detect hands in the frame
-    hands = hand_cascade.detectMultiScale(gray, 1.3, 5)
-
-    # If a hand is detected
-    if len(hands) > 0:
-        # Get the hand bounding box
-        (x, y, w, h) = hands[0]
-
-        # Update the canvas pointer position
-        update_pointer_position(x + w // 2, y + h // 2)
-
-    # Schedule the next frame processing
-    root.after(30, process_frame)
-
-# Start the frame processing
-process_frame()
-
-# Start the Tkinter main loop
-root.mainloop()
+if __name__ == '__main__':
+    display_multiple_windows()
