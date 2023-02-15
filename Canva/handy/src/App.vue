ff10f9f02cc1b3256e08a55e655f7c816a1d50a2
Angela Maria Gigi <angelamariagigi@gmail.com>
2023-02-16 00:49:30 +0530

    new canvas added with paper js

---
+++ Canva/handy/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <div class="container-fluid">
+      <Master/>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Master from "./pages/MasteR";
+export default {
+  name: 'App',
+  components: {
+    Master
+  }
+}
+</script>
---
+++ Canva/handy/src/pages/MasteR.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <!-- button toggles -->
+        <div class="row mt-5">
+            <div class="col-5" />
+            <div class="col-2">
+                <button class="btn btn-primary btn-block" @click.prevent="reset">CLEAR</button>
+            </div>
+            <div class="col-5"/>
+        </div>
+
+        <!-- canvas single -->
+        <div class="row mt-5">
+            <div class="col-2"/>
+            <div class="col-8">
+                <Canvas :canvas-id="'canvas-one'" ref="childCanvas"/>
+            </div>
+            <div class="col-2"/>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Canvas from "../components/CanvaS";
+    export default {
+        name: "MasteR",
+        data: () => ({
+            buttons: ['Simple', 'Separate', 'Replicated'],
+            single: true
+        }),
+        methods: {
+            reset() {
+                this.$refs.childCanvas.reset();
+            }
+        },
+        components: {
+            Canvas
+        }
+    }
+</script>
+
+<style scoped>
+</style>
