421ca0c21d11b6a0e372c19e00a82a041b9b8653
Angela Maria Gigi <angelamariagigi@gmail.com>
2023-02-16 10:38:51 +0530

    Vuetify added

--- Canva/handy/src/main.js
+++ Canva/handy/src/main.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueKonva from 'vue-konva';
+import vuetify from './plugins/vuetify'
 
 Vue.use(VueKonva);
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
