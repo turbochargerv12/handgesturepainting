05b6a6bb13d400eae34dcdba55783bcd5c18c9fd
Roshan Reji <roshanreji2077@gmail.com>
2023-05-02 10:02:19 -0700

    hmm

--- Canva/handy/src/components/WebCam.vue
+++ Canva/handy/src/components/WebCam.vue
@@ -3,34 +3,22 @@
 </template>
 
 <script>
+import flvjs from 'flv.js';
+
 export default {
   mounted() {
     navigator.mediaDevices.getUserMedia({ video: true })
       .then(stream => {
         const video = this.$refs.video;
         video.srcObject = stream;
-        const socket = new WebSocket('ws://localhost:5000/video');
-        const CHUNK_SIZE = 1024 * 1024; // 1MB
-        let offset = 0;
-        const sendChunk = () => {
-          const stream = video.captureStream();
-          const recorder = new MediaRecorder(stream, { mimeType: 'video/webm' });
-          
-          recorder.ondataavailable = function (event) {
-            socket.send(event.data);
-          };
-
-          recorder.start();
-
-          setTimeout(() => {
-            recorder.stop();
-            if (offset < stream.size) {
-              sendChunk();
-            }
-          }, 100);
-          offset += CHUNK_SIZE;
-        };
-        sendChunk();
+        const flvPlayer = flvjs.createPlayer({
+          type: 'flv',
+          isLive: true,
+          url: 'rtmp://your-streaming-server:1935/live/stream-name',
+        });
+        flvPlayer.attachMediaElement(video);
+        flvPlayer.load();
+        flvPlayer.play();
       });
   }
 }
